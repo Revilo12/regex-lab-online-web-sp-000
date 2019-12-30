179ef22ad89ff5482aa5e36db10f5a755009b2be
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-30 15:21:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,16 @@
 def starts_with_a_vowel?(word)
   !! word.match(/^[aeiou]\w+/) 
-  #Theory
+=begin
+  Theory
     !! turns it into a boolean value (could alternatively use match?)
     The word is being acted on with match which returns the first thing that works from the String
     The upcarrot tells it that it has to be at the start 
     The vowels in the brackets say that the next thing has to be a vowel.
     The lower case w means that it can be any character that isn't specilal. 
+    The plus means that any number of these can occur
+=end
     
 end
-
-puts starts_with_a_vowel?("Cat")
 
 def words_starting_with_un_and_ending_with_ing(text)
 
