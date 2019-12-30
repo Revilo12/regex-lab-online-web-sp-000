d49d1b9a5cc0b5d6975f179b8a0bee37eed29900
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-30 15:12:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  !!word.match(/[aeiou]./) #a vowel plus any combination of characters
+  !!word.match(/[aeiou]$/) #a vowel plus any combination of characters
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
